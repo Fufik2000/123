c59a62815c023564dadb7c95c30146eba3e53153
Fufik2000 <addaads945@gmail.com>
2022-10-24 07:59:45 +0000

    23.3

---
+++ massiv/23.3.php
@@ -0,0 +1,9 @@
+<?php
+    $user = ['name' => 'Fufik', 'surname' => 'Jumal', 'patronymic' => 2000];
+    echo $user['name'];
+    echo ' ';
+    echo $user['surname'];
+    echo ' ';
+    echo $user['patronymic'];
+    echo ' ';
+?>
